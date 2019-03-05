c6749cd500e2ebaa329d84b0916620621d1ee6ff
Eivano <evg212009@yandex.ru>
2019-03-06 00:48:17 +0800

    1.3. JavaDoc.Code convention [#118373]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,6 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author Evgenii Ivanov (evg212009@yandex.ru)
+*/
+
 public class Calculate {
+
+/**
+* Main.
+* Конструктор, вывод строки в консоль.
+* @param args - args.
+*/
 
     public static void main(String[] args) {
         System.out.println("Hello World");
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,9 @@
+/**
+*Package for calculate task.
+*
+*@author Evgenii Ivanov (evg212009@yandex.ru)
+*@version 1
+*@since 06.03.2019
+*/
+
+package ru.job4j.calculate;
