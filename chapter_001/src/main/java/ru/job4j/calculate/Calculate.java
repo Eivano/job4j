49ce507b031ea5a95242c42fbd61eca8211d3967
Eivano <evg212009@yandex.ru>
2019-03-06 13:09:16 +0800

    1.4. JUnit[#116024]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,9 +1,11 @@
 package ru.job4j.calculate;
 
 /**
-* Calculate
+* Calculate.
 *
 * @author Evgenii Ivanov (evg212009@yandex.ru)
+* @version 1
+* @since 06.03.2019
 */
 
 public class Calculate {
@@ -16,6 +18,15 @@ public class Calculate {
 
     public static void main(String[] args) {
         System.out.println("Hello World");
+	}
+	
+	/**
+	* Method echo.
+	* @param name Evgenii Ivanov.
+	* @return Echo plus Evgenii Ivanov.
+	*/
+	public String echo(String name) {
+		return "Echo, echo, echo : " + name;
 	}
 	
 }
---
+++ chapter_001/src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+* Test.
+*
+* @author Evgenii Ivanov (evg212009@yandex.ru)
+* @version 1
+* @since 06.03.2019
+*/
+
+public class CalculateTest {
+/**
+* Test echo.
+*/
+@Test
+public void whenTakeNameThenThreeEchoPlusName(){
+  String input = "Evgenii Ivanov";
+  String expect = "Echo, echo, echo : Evgenii Ivanov";
+  Calculate calc = new Calculate();
+  String result = calc.echo(input);
+  assertThat(result, is(expect));
+
+}
+
+
+
+}
