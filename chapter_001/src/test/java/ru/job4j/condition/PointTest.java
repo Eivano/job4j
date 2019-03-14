6113c05574aef05efbd00a3564481b25381455bf
Eivano <evg212009@yandex.ru>
2019-03-14 19:41:15 +0800

    3.4. Расстояние между точками в системе координат [#116015]

--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -9,7 +9,7 @@ import static org.junit.Assert.assertThat;
         public void whenZeroAndTenThenTen() {
             Point point = new Point();
             double result = point.distance(0, 0, 0, 10);
-            assertThat(result, is(10));
+            assertThat(result, is(10.0));
         }
 		
     }
