b51cc264392780af52993cded8c6da92baade86b
Eivano <evg212009@yandex.ru>
2019-03-25 23:13:21 +0800

    4.2.Максимум из двух чисел.[#115987]

--- chapter_001/src/main/java/ru/job4j/condition/Max.java
+++ chapter_001/src/main/java/ru/job4j/condition/Max.java
@@ -1,10 +1,38 @@
 package ru.job4j.condition;
 
+/**
+* @author Evgenii Ivanov (evg212009@yandex.ru).
+* @version 1.
+* @since 25.03.2019.
+* Вычисление максимума из двух чисел и сравнение двух чисел.
+*/
 public class Max {
+	/**
+	* @Method max.
+	* @param result Тернарное условие.
+	* @return Первое максимальное число.
+	*/
 	public int max(int left, int right) {
 		int result = left < right ? right : left;
 		return result;
 	}
-
+	/**
+	* @Method max2.
+	* @param result Тернарное условие.
+	* @return Второе максимальное число.
+	*/
+	public int max2(int left, int right) {
+		int result = left > right ? left : right;
+		return result;
+	}
+	/**
+	* @Method equally.
+	* @param result Тернарное условие.
+	* @return Оба числа одинаковы.
+	*/
+	public int eq(int left, int right) {
+		int result = left == right ? left : left;
+		return result;
+	}
 
 }
--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -4,23 +4,40 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+/**
+* Test.
+*
+* @author Evgenii Ivanov (evg212009@yandex.ru).
+* @version 1.
+* @since 25.03.2019.
+*/
+
 public class MaxTest {
+	/**
+	* Test max.
+	*/
     @Test
     public void whenMax1To2Then2() {
         Max max = new Max();
         int result = max.max(1, 2);
         assertThat(result, is(2));
     }
+	/**
+	* Test max2.
+	*/
     @Test
     public void whenMax2To1Then2() {
-        Max max = new Max();
-        int result = max.max(2, 1);
+        Max max2 = new Max();
+        int result = max2.max2(2, 1);
         assertThat(result, is(2));
     }
+	/**
+	* Test equally.
+	*/
     @Test
-    public void whenEqually1To2Then1() {
-        Max max = new Max();
-        int result = max.max(1, 2);
+    public void whenEqually1To1Then1() {
+        Max eq = new Max();
+        int result = eq.eq(1, 1);
         assertThat(result, is(1));
     }
 }
